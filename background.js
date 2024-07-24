5fb14caae68f900d64d996dbdb4fd1c838801d2f
Parikshit <prikshitbhardwaj6@gmail.com>
2024-07-25 02:04:12 +0530

    .

--- background.js
+++ background.js
@@ -1,17 +1,20 @@
-chrome.alarms.create("workReminderAlarm", { periodInMinutes: 10 }); // Adjust period as needed
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('Service worker registered');
+    chrome.alarms.create("workReminderAlarm", { periodInMinutes: 10 }); // Adjust period as needed
+});
 
-chrome.alarms.onAlarm.addListener(function(alarm) {
-  if (alarm.name === "workReminderAlarm") {
-    chrome.notifications.create({
-      type: 'basic',
-      iconUrl: 'icon.png',
-      title: 'Reminder',
-      message: 'Time to get back to work!',
-      priority: 1
-    });
+chrome.alarms.onAlarm.addListener((alarm) => {
+    if (alarm.name === "workReminderAlarm") {
+        chrome.notifications.create({
+            type: 'basic',
+            iconUrl: 'icon.png',
+            title: 'Reminder',
+            message: 'Time to get back to work!',
+            priority: 1
+        });
 
-    // Play a notification sound
-    const audio = new Audio('notification_sound.mp3');
-    audio.play();
-  }
+        // Play a notification sound
+        const audio = new Audio('notification_sound.mp3');
+        audio.play();
+    }
 });
